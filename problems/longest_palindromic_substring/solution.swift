e2d0f66bfd1a4f2d52ef1325d2033a2dcf9dcec1
Mohit Athwani <mohit.athwani@gmail.com>
2018-04-10 18:03:52 +0000

    Sync LeetCode submission - Longest Palindromic Substring (swift)

---
+++ problems/longest_palindromic_substring/solution.swift
@@ -0,0 +1,41 @@
+class Solution {
+    func longestPalindrome(_ s:String) -> String {
+        if(s.count == 1 || s.isEmpty) {
+        return s
+    }
+    
+    var resultMatrix = [[Bool]](repeatElement([Bool](repeatElement(false, count: s.count)), count: s.count))
+    var charArray = Array(s)
+    
+    for i in 0..<s.count {
+        resultMatrix[i][i] = true
+    }
+    
+    var startIndex = 0
+    var maxLen = 0
+    
+    for i in (0...s.count - 2).reversed() {
+        for j in (i + 1)..<s.count {
+            if(charArray[i] != charArray[j]) {
+                resultMatrix[i][j] = false;
+            }
+            else {
+                resultMatrix[i][j] = true && (resultMatrix[i + 1][j - 1] || j - i < 3)
+                
+                if resultMatrix[i][j] == true && (j - i + 1) > maxLen {
+                    startIndex = i
+                    maxLen = j - i + 1
+                }
+            }
+        }
+    }
+    
+    if maxLen == 0 {
+        return String(charArray[0])
+    }
+    
+    let beginningIndex = s.index(s.startIndex, offsetBy: startIndex)
+    let endIndex = s.index(beginningIndex, offsetBy: maxLen)
+    return String(s[beginningIndex..<endIndex])
+}
+}
