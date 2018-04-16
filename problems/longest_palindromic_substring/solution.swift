37005b0d8c578efe186fdac84e322a88dc93f166
Mohit Athwani <mohit.athwani@gmail.com>
2018-04-16 19:09:35 +0000

    Sync LeetCode submission - Longest Palindromic Substring (swift)

--- problems/longest_palindromic_substring/solution.swift
+++ problems/longest_palindromic_substring/solution.swift
@@ -1,41 +1,32 @@
 class Solution {
     func longestPalindrome(_ s:String) -> String {
-        if(s.count == 1 || s.isEmpty) {
-        return s
-    }
+    var start = 0
+    var end = 0
+
+    let charArray = Array(s)
     
-    var resultMatrix = [[Bool]](repeatElement([Bool](repeatElement(false, count: s.count)), count: s.count))
-    var charArray = Array(s)
-    
-    for i in 0..<s.count {
-        resultMatrix[i][i] = true
-    }
-    
-    var startIndex = 0
-    var maxLen = 0
-    
-    for i in (0...s.count - 2).reversed() {
-        for j in (i + 1)..<s.count {
-            if(charArray[i] != charArray[j]) {
-                resultMatrix[i][j] = false;
-            }
-            else {
-                resultMatrix[i][j] = true && (resultMatrix[i + 1][j - 1] || j - i < 3)
-                
-                if resultMatrix[i][j] == true && (j - i + 1) > maxLen {
-                    startIndex = i
-                    maxLen = j - i + 1
-                }
-            }
+    for i in charArray.indices {
+        let length = max(expandAround(i, j: i, charArray: charArray), expandAround(i, j: i + 1, charArray: charArray))
+        if length > end - start {
+            start = i - (length - 1) / 2
+            end = i + (length / 2)
         }
     }
     
-    if maxLen == 0 {
-        return String(charArray[0])
+    let beginningIndex = s.index(s.startIndex, offsetBy: start)
+    let endIndex = s.index(s.startIndex, offsetBy: end)
+    return String(s[beginningIndex...endIndex])
+}
+
+func expandAround(_ i: Int, j: Int, charArray:[Character]) -> Int {
+    var leftIndex = i
+    var rightIndex = j
+    
+    while leftIndex >= 0 && rightIndex < charArray.count && charArray[leftIndex] == charArray[rightIndex] {
+        leftIndex -= 1
+        rightIndex += 1
     }
     
-    let beginningIndex = s.index(s.startIndex, offsetBy: startIndex)
-    let endIndex = s.index(beginningIndex, offsetBy: maxLen)
-    return String(s[beginningIndex..<endIndex])
+    return rightIndex - leftIndex - 1
 }
 }
