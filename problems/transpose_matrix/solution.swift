3fb080075312581618c30fa0ff860eb7acf5cac3
Mohit Athwani <mohit.athwani@gmail.com>
2018-07-08 02:38:04 +0000

    Sync LeetCode submission - Transpose Matrix (swift)

---
+++ problems/transpose_matrix/solution.swift
@@ -0,0 +1,13 @@
+class Solution {
+func transpose(_ A: [[Int]]) -> [[Int]] {
+  var result = [[Int]](repeating: [Int](repeating: 0, count: A.count), count: A[0].count)
+  
+  for i in stride(from: 0, to: A.count, by: 1) {
+    for j in stride(from: 0, to: A[0].count, by: 1) {
+      result[j][i] = A[i][j]
+    }
+  }
+  
+  return result
+}
+}
