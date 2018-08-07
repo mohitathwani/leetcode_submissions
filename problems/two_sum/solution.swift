73fa0ef11e5426cfeb6517043df2cbfbd95cd9fa
Mohit Athwani <mohit.athwani@gmail.com>
2018-08-07 22:45:30 +0000

    Sync LeetCode submission - Two Sum (swift)

--- problems/two_sum/solution.swift
+++ problems/two_sum/solution.swift
@@ -1,14 +1,14 @@
 class Solution {
     func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
-        var seenNumbers = [Int:Int]()
-        
-        for (index, val) in nums.enumerated() {
-            if seenNumbers[target - val] != nil {
-                return [seenNumbers[target - val]!, index]
-            }
-            seenNumbers[val] = index
+    var seenNumbers = [Int:Int]()
+    
+    for (index, val) in nums.enumerated() {
+        if seenNumbers[target - val] != nil {
+            return [seenNumbers[target - val]!, index]
         }
-        
-        return [Int]()
+        seenNumbers[val] = index
     }
+    
+    return [Int]()
+}
 }
