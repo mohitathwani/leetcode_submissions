ce7a1e6a60e31b589901875bf69b4421db98bce4
Mohit Athwani <mohit.athwani@gmail.com>
2020-11-25 19:58:11 +0000

    Sync LeetCode submission - Find First and Last Position of Element in Sorted Array (cpp)

--- problems/find_first_and_last_position_of_element_in_sorted_array/solution.cpp
+++ problems/find_first_and_last_position_of_element_in_sorted_array/solution.cpp
@@ -1,23 +1,42 @@
+/**
+0 1 2 3 4 5 6
+1 2 3 3 3 4 5
+*/
 class Solution {
 public:
+    int search(vector<int>& nums, int target, bool searchLeft) {
+        int lo = 0;
+        int hi = nums.size() - 1;
+        int pos = -1;
+        
+        while (lo <= hi) {
+            int mid = lo + ((hi - lo) / 2);
+            
+            if (nums[mid] == target) {
+                pos = mid;
+                if (searchLeft) {
+                    hi = mid - 1;
+                }
+                else {
+                    lo = mid + 1;
+                }
+            }
+            else if (nums[mid] < target) {
+                lo = mid + 1;
+            }
+            else {
+                hi = mid - 1;
+            }
+        }
+        
+        return pos;
+    }
+    
     vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int> result = {-1, -1};
+        int left = search(nums, target, true);
+        int right = search(nums, target, false);
         
-        for(int i = 0; i < nums.size(); i++) {
-            if (nums[i] == target) {
-                result[0] = i;
-                result[1] = i;
-                break;
-            }
-        }
-        
-        for (int j = nums.size() - 1; j > result[0]; j--) {
-            if (nums[j] == target) {
-                result[1] = j;
-                break;
-            }
-        }
-        
+        vector<int> result = {left, right};
         return result;
     }
 };
